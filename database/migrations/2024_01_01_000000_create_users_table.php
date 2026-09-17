01a5f7376bd63058a85a6d39ef6bf93aeb309550
bbhcstech <karanmahilang05@gmail.com>
2026-09-17 14:55:26 +0530

    user table migration updated

--- database/migrations/2024_01_01_000000_create_users_table.php
+++ database/migrations/2024_01_01_000000_create_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
             $table->string('email', 150)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['admin', 'publisher', 'customer'])->default('customer');
+            $table->enum('role', ['super_admin', 'admin', 'publisher', 'customer'])->default('customer');
             $table->enum('status', ['active', 'suspended'])->default('active');
             $table->rememberToken();
             $table->timestamps();
